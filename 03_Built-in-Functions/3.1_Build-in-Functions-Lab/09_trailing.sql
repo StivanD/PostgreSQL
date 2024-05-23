64ed8da4d3ca0258de457f2d5afd8f5dfa1bad68
Stivan Dimitrov <stivandimitrov27@gmail.com>
2024-05-23 20:39:50 +0300

    Added solution

--- 03_Built-in-Functions/3.1_Build-in-Functions-Lab/09_trailing.sql
+++ 03_Built-in-Functions/3.1_Build-in-Functions-Lab/09_trailing.sql
@@ -1,15 +1,15 @@
 SELECT
 	continent_name,
-	TRIM(LEADING FROM continent_name)
+	TRIM(TRAILING FROM continent_name)
 FROM
 	continents;
-	
+    
 /*
 DOES THE SAME THING
 
 SELECT
 	continent_name,
-	LTRIM(continent_name)
+	RTRIM(continent_name)
 FROM
 	continents;
 */
