fa7f87e21337a52335e5a773073f57e948a65791
Stivan Dimitrov <stivandimitrov27@gmail.com>
2024-05-23 21:43:00 +0300

    Added solution

---
+++ 03_Built-in-Functions/3.1_Build-in-Functions-Lab/21_billing_day.sql
@@ -0,0 +1,11 @@
+-- THIS TASK IS NOT SUBMITTED IN JUDGE
+
+ALTER TABLE
+	bookings
+ADD COLUMN
+	billing_day TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP;
+
+SELECT
+	TO_CHAR(billing_day, 'DD "Day" MM "Month" YYYY "Year" HH24:MI:SS') AS "Billing Day"
+FROM
+	bookings;
