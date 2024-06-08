b1c6d3c08cc3c4567f803f30f55041cf2e8ded0f
StivanD <stivandimitrov27@gmail.com>
2024-06-08 11:47:37 +0300

    Added solution

---
+++ 07_Database-Programmability-and-Transactions/7.2_Database-Programmability-and-Transactions-Exercise/03_user_defined_function_is_word_comprised.sql
@@ -0,0 +1,15 @@
+CREATE OR REPLACE FUNCTION fn_is_word_comprised (
+	set_of_letters VARCHAR(50),
+	word VARCHAR(50)
+) RETURNS BOOLEAN
+AS
+$$
+BEGIN
+	RETURN TRIM(LOWER(word), LOWER(set_of_letters)) = '';
+END;
+$$
+LANGUAGE plpgsql;
+
+-- SELECT * FROM fn_is_word_comprised('ois tmiah%f', 'halves'); -> SHOULD RETURN FALSE
+
+-- SELECT * FROM fn_is_word_comprised('bobr', 'Rob'); -> SHOULD RETURN TRUE
