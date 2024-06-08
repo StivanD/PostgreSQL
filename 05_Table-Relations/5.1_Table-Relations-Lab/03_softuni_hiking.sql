2a1e497657309c19a85f4157681569297ed24034
StivanD <stivandimitrov27@gmail.com>
2024-06-08 09:58:10 +0300

    Added solution

---
+++ 05_Table-Relations/5.1_Table-Relations-Lab/03_softuni_hiking.sql
@@ -0,0 +1,15 @@
+SELECT
+	r.start_point,
+	r.end_point,
+	r.leader_id,
+	CONCAT(
+		c.first_name,
+		' ',
+		c.last_name
+	) AS leader_name
+FROM
+	routes as r
+JOIN
+	campers as c
+ON
+	r.leader_id = c.id;
