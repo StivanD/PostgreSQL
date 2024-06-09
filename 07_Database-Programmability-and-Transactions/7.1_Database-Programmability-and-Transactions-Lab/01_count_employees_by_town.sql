c9fc0c6f7627da50cc272113cfa45e506afc8732
StivanD <stivandimitrov27@gmail.com>
2024-06-09 14:21:32 +0300

    Added solution

---
+++ 07_Database-Programmability-and-Transactions/7.1_Database-Programmability-and-Transactions-Lab/01_count_employees_by_town.sql
@@ -0,0 +1,27 @@
+CREATE OR REPLACE FUNCTION fn_count_employees_by_town (
+	town_name VARCHAR(20)
+) RETURNS INT
+AS
+$$
+DECLARE 
+	result INT;
+BEGIN
+	SELECT
+		COUNT(*) AS count
+	FROM
+		towns AS t
+	JOIN
+		addresses AS a
+	USING
+		(town_id)
+	JOIN
+		employees AS e
+	USING
+		(address_id)
+	WHERE
+		t.name = town_name INTO result;
+
+	RETURN result;
+END;
+$$
+LANGUAGE plpgsql;
