657d174279daacaaaf4df706457a9163629b1d84
StivanD <stivandimitrov27@gmail.com>
2024-06-09 14:24:43 +0300

    Added solution

---
+++ 07_Database-Programmability-and-Transactions/7.1_Database-Programmability-and-Transactions-Lab/02_employees_promotion.sql
@@ -0,0 +1,22 @@
+CREATE OR REPLACE PROCEDURE sp_increase_salaries(
+	department_name VARCHAR(50)
+)
+AS
+$$
+BEGIN
+	UPDATE
+		employees
+	SET
+		salary = salary * 1.05
+	WHERE
+		department_id = (
+			SELECT
+				department_id
+			FROM
+				departments as d
+			WHERE
+				department_name = d.name
+		);
+END;		
+$$
+LANGUAGE plpgsql;
