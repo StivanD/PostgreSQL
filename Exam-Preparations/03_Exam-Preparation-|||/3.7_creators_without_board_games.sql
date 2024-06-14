09979f1b72aab1eef1f9e90237c6ab657ba75bb6
StivanD <stivandimitrov27@gmail.com>
2024-06-15 00:39:50 +0300

    Added solution

---
+++ Exam-Preparations/03_Exam-Preparation-|||/3.7_creators_without_board_games.sql
@@ -0,0 +1,19 @@
+SELECT
+	id,
+	CONCAT(
+		first_name,
+		' ',
+		last_name
+	) AS creator_name,
+	email
+FROM
+	creators AS c
+WHERE
+	id NOT IN (
+		SELECT
+			creator_id
+		FROM
+			creators_board_games
+	)
+ORDER BY
+	creator_name;
